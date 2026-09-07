6eba8acc5f7de1e58a463ce8f15060b1af5995e6
jaacquees <52385807+jaacquees@users.noreply.github.com>
2026-09-07 13:55:25 -0400

    Build dedicated adjust route

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
   base: '/vayetsei/',
-  publicDir: 'static'
+  publicDir: 'static',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        adjust: resolve(process.cwd(), 'adjust/index.html')
+      }
+    }
+  }
 });
